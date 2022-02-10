7cbafbd577cd42d62081aa4ac7776b95af25836d
ethanhinton <eth4n.hinton@gmail.com>
2022-02-10 18:22:38 +0000

    Added comments.

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ import json
 
 TICKER = "AMZN"
 
+# Create service object with API
 with build("youtube", "v3", developerKey=API_KEY) as service:
+
+    # Create objects for search query and video query
     search = service.search()
     videos = service.videos()
 
+    # Generate Search request object
     request = search.list(part=["snippet"], q=f"{TICKER} stock", order="relevance")
 
+    # Call API and obtain response
     try:
         response = request.execute()
     except HttpError as e:
