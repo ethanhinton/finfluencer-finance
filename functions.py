bdc0131bdaa1f0343809025fb804fe2424dd9965
ethanhinton <eth4n.hinton@gmail.com>
2022-02-14 15:28:50 +0000

    Added functionality for fetching transcripts of videos.

--- functions.py
+++ functions.py
@@ -1,7 +1,11 @@
 from youtube_transcript_api import YouTubeTranscriptApi as trans
+from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFound
+from googleapiclient.errors import HttpError
+
 
 def extract_vid_data(video):
     out = [
+        video["id"],
         video["snippet"]["title"],
         video["snippet"]["description"],
         (video["snippet"]["tags"] if "tags" in video["snippet"].keys() else ""),
@@ -17,8 +21,21 @@ def extract_vid_data(video):
     return out
 
 def get_transcript(video_id):
-    transcript = trans.list_transcripts(video_id).find_transcript(language_codes=["en"]).fetch()
-
-    transcript_text = " ".join(list(map(lambda x: x["text"], transcript)))
-
+    print(f"Fetching transcript for {video_id}...")
+    try:
+        transcript = trans.list_transcripts(video_id).find_transcript(language_codes=["en"]).fetch()
+        transcript_text = " ".join(list(map(lambda x: x["text"], transcript)))
+        print("Done")
+    except (NoTranscriptFound, TranscriptsDisabled):
+        print("No English transcript available")
+        return "No transcript"
     return transcript_text
+
+def get_data(request):
+    try:
+        response = request.execute()
+        result = response["items"]
+    except HttpError as e:
+        print(f"Error status code : {e.status_code}, reason : {e.reason}")
+        quit()
+    return result
--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from keys import *
 from functions import *
 import csv
 from googleapiclient.discovery import build
-
-
-
+import pandas as pd
 
 TICKER = "AMZN"
 
@@ -17,10 +15,13 @@ with build("youtube", "v3", developerKey=API_KEY) as service:
     channels = service.channels()
 
     # Generate Search request object
-    request = search.list(part=["snippet"], q=f"{TICKER} stock", order="relevance", maxResults=50)
+    request = search.list(part=["snippet"], q=f"{TICKER} stock", order="relevance", maxResults=50, type="video")
 
     # Call API and obtain response
     search_result = get_data(request)
+    # for result in search_result:
+    #     print(result["id"].keys())
+    #     print("")
 
     # Extract video ID and channel ID for each video in the search result
     vid_ids = list(map(lambda x : x["id"]["videoId"], search_result))
@@ -29,18 +30,19 @@ with build("youtube", "v3", developerKey=API_KEY) as service:
     # print(channel_ids)
     
     # Generate video and channel request objects
-    vid_request = videos.list(part=["snippet", "statistics", "contentDetails"], id=vid_ids)
+    vid_request = videos.list(part=["id","snippet", "statistics", "contentDetails"], id=vid_ids)
     channel_request = channels.list(part=["statistics"], id=channel_ids, maxResults=50)
 
-    # Call APIs for video and channel info
+    # Call APIs for video, channel info, and transcripts
     vid_data = get_data(vid_request)
-    channel_data = get_data(channel_request)
+    # channel_data = get_data(channel_request)
+    transcript_data = list(map(get_transcript, vid_ids))
 
-    # Extract data and save to csv file
+    # Extract data, collect into a dataframe, and save to csv file
     body = list(map(extract_vid_data, vid_data))
-    headers = ["Title", "Description", "Tags", "Publish Date", "Thumbnail", "Duration", "Views", "Likes", "Comments", "Channel Name"]
-    
-    with open("output.csv", "w", encoding="utf-8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(headers)
-        writer.writerows(body)
+    headers = ["VideoID", "Title", "Description", "Tags", "Publish Date", "Thumbnail", "Duration", "Views", "Likes", "Comments", "Channel Name"]
+    df = pd.DataFrame(data=body, columns=headers)
+    df["Transcript"] = transcript_data
+    df.set_index("VideoID", inplace=True)
+
+    df.to_csv("output.csv")
