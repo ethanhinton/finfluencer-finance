7fe2b36f46a4d0716450cb0dff8faf76b4b772f8
ethanhinton <eth4n.hinton@gmail.com>
2022-02-14 14:04:52 +0000

    Moved some code to functions.py to neaten the code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from keys import *
+from functions import *
 import csv
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+
 
 
 
@@ -19,13 +20,7 @@ with build("youtube", "v3", developerKey=API_KEY) as service:
     request = search.list(part=["snippet"], q=f"{TICKER} stock", order="relevance", maxResults=50)
 
     # Call API and obtain response
-    try:
-        response = request.execute()
-    except HttpError as e:
-        print(f"Error status code : {e.status_code}, reason : {e.reason}")
-        quit()
-
-    search_result = response["items"]
+    search_result = get_data(request)
 
     # Extract video ID and channel ID for each video in the search result
     vid_ids = list(map(lambda x : x["id"]["videoId"], search_result))
@@ -38,37 +33,10 @@ with build("youtube", "v3", developerKey=API_KEY) as service:
     channel_request = channels.list(part=["statistics"], id=channel_ids, maxResults=50)
 
     # Call APIs for video and channel info
-    try:
-        vid_response = vid_request.execute()
-        vid_data = vid_response["items"]
-    except HttpError as e:
-        print(f"Error status code : {e.status_code}, reason : {e.reason}")
-        quit()
-    
-    try:
-        channel_response = channel_request.execute()
-        channel_data = channel_response["items"] 
-    except HttpError as e:
-        print(f"Error status code : {e.status_code}, reason : {e.reason}")
-        quit()
-    
-    # Extract data and save to csv file
-    def extract_vid_data(video):
-        out = [
-            video["snippet"]["title"],
-            video["snippet"]["description"],
-            (video["snippet"]["tags"] if "tags" in video["snippet"].keys() else ""),
-            video["snippet"]["publishedAt"],
-            video["snippet"]["thumbnails"]["high"]["url"],
-            video["contentDetails"]["duration"],
-            video["statistics"]["viewCount"],
-            (video["statistics"]["likeCount"] if "likeCount" in video["statistics"].keys() else ""),
-            video["statistics"]["commentCount"],
-            video["snippet"]["channelTitle"],
-            # channel["statistics"]["subscriberCount"]
-        ]
-        return out
+    vid_data = get_data(vid_request)
+    channel_data = get_data(channel_request)
 
+    # Extract data and save to csv file
     body = list(map(extract_vid_data, vid_data))
     headers = ["Title", "Description", "Tags", "Publish Date", "Thumbnail", "Duration", "Views", "Likes", "Comments", "Channel Name"]
     
