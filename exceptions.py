1b86530d1cc69727a1d0e3a5d3d29adbd2f9356d
ethanhinton <eth4n.hinton@gmail.com>
2022-03-10 17:49:16 +0000

    Added CommentsDisabledError for when a keyerror is thrown because a video has disabled comments

--- exceptions.py
+++ exceptions.py
@@ -6,3 +6,23 @@ class APIError(Exception):
 
 class QuotaExceededError(APIError):
     pass
+
+class CommentsDisabledError(APIError):
+    pass
+
+# When an error is raised internally with the YouTube API, it does not throw an HTTP error on the request, instead it returns a response with the error cause as a
+# dictionary. This will cause a KeyError to be raised later on in the code in most circumstances
+# This function can be run whenever there is a KeyError to diagnose the problem and raise the correct error.
+def check_keyerror_cause(response):
+    if type(response) == list:
+        response = response[0]
+    if "errors" in response.keys():
+        reason = response["error"]["errors"][0]["reason"]
+        if reason == "quotaExceeded":
+            raise QuotaExceededError("The daily API quota has been exceeded")
+        elif reason == "commentsDisabled":
+            raise CommentsDisabledError("Comments are disabled on this video")
+        else:
+            raise APIError(reason)
+    else:
+        raise KeyError
