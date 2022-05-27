8f1acda50b2212c322b5d0d33ee93e48af8739e9
ethan <ethan.hinton@gmail.com>
2022-05-27 19:17:39 +0100

    Renamed files

--- async_youtube_test.py
+++ async_youtube_test.py
@@ -1,7 +1,6 @@
 import asyncio
-import aiohttp
 from keys import API_KEY
-from exceptions import QuotaExceededError, APIError, CommentsDisabledError, check_keyerror_cause
+from exceptions import CommentsDisabledError, check_keyerror_cause
 from functions import extract_channel_data, extract_vid_data
 
 class AsyncYoutube:
@@ -11,26 +10,24 @@ class AsyncYoutube:
         self.api_key = api_key
         self.base_url = "https://www.googleapis.com/youtube/v3"
 
-    # Gets video and channel ids for a list of queries and video duration filters
-    async def get_ids(self, queries, max_results=50, order="date", video_duration=["any"]):
-        # Call API for multiple duration filters for each query in the query input list (returns a list of lists of video ids)
-        tasks = [self.ids_multi_duration(query, max_results=max_results, order=order, durations=video_duration) for query in queries]
-        responses = await asyncio.gather(*tasks)
+    # Gets video and channel ids for a single query and video duration filters
+    async def get_ids(self, query, max_results=50, order="date", video_duration=["any"]):
+        # Call API for multiple duration filters for the query
+        response = await self.ids_multi_duration(query, max_results=max_results, order=order, durations=video_duration)
 
         # Unpack lists and output two lists of video and channel ids for all videos in all queries 
         vid_ids = []
         channel_ids = []
         tickers = []
 
-        for i, response in enumerate(responses):
-            ticker = queries[i].split(" ")[0]
-            tickers.extend([ticker]*len(response[0]))
-            vid_ids.extend(response[0])
-            channel_ids.extend(response[1])
+        ticker = query.split(" ")[0]
+        tickers.extend([ticker]*len(response[0]))
+        vid_ids.extend(response[0])
+        channel_ids.extend(response[1])
         return vid_ids, channel_ids, tickers
 
     # Gets video and channel ids for a list of duration filters
-    async def ids_multi_duration(self, query, max_results=50, order="date", durations=["short", "medium", "long"]):
+    async def ids_multi_duration(self, query, max_results=50, order="date", durations=["any"]):
         # Call API to search for query with each duration filter
         tasks = [self.session.get(f"{self.base_url}/search?part=snippet&maxResults={max_results}&q={query.replace(' ', '%20')}&type=video&order={order}&videoDuration={duration}&key={self.api_key}") for duration in durations]
     
@@ -72,6 +69,7 @@ class AsyncYoutube:
 
         return list(map(lambda x: x['snippet']['topLevelComment']['snippet']['textOriginal'], items))
     
+    # Runs get_comments for a list of video ids
     async def get_comments_multi_videos(self, vid_ids):
         tasks = [self.get_comments(vid_id) for vid_id in vid_ids]
 
@@ -97,10 +95,7 @@ class AsyncYoutube:
         # Get rid of useless metadata about the API calls and add individual channel information to a list "items"
         items = []
         for result in results:
-            try:
-                items.extend(result["items"])
-            except KeyError as e:
-                check_keyerror_cause(result)
+            items.extend(result["items"])
 
         # The API removes duplicate channel info outputs for duplicate channel ids in a single call, however this does not happen when the API is called multiple times
         # therefore we must remove duplicate channel data before we return it
@@ -120,6 +115,7 @@ class AsyncYoutube:
 
         tasks = [self.session.get(f"{self.base_url}/videos?part=id&part=statistics&part=snippet&part=contentDetails&maxResults=50&id={ids}&key={self.api_key}") for ids in req_strings]
 
+
         responses = await asyncio.gather(*tasks)
 
         results = [await response.json() for response in responses]
@@ -127,10 +123,55 @@ class AsyncYoutube:
         # Get rid of useless metadata about the API calls and add individual video information to a list "items"
         items = []
         for result in results:
-            try:
-                items.extend(result["items"])
-            except KeyError as e:
-                check_keyerror_cause(result)
+            items.extend(result["items"])
 
         # Extract the data that we want from the set of all video data for each video and return as a list of lists of single video data
         return list(map(extract_vid_data, items))
+
+    # Calls all relevant async functions to get all info for one video
+    async def get_info_for_query(self, query, include_comments=True, video_duration=["any"]):
+        vid_ids, channel_ids, tickers = await self.get_ids(query, video_duration=video_duration)
+        subs = await self.get_subscribers(channel_ids)
+        vids = await self.get_video_data(vid_ids)
+
+        if include_comments:
+            comments = await self.get_comments_multi_videos(vid_ids)
+        else:
+            comments = False
+
+        return vid_ids, channel_ids, tickers, vids, subs, comments
+    
+    # Calls get_info_for_query asynchronously for all queries retrieving all information 
+    async def get_output_all_queries(self, queries, include_comments=True, video_duration=["any"]):
+        tasks = [asyncio.create_task(self.get_info_for_query(query, include_comments=include_comments, video_duration=video_duration)) for query in queries]
+
+        done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
+        
+        if pending:
+            print(f"Number of queries retrieved : {len(done) - 1}")
+
+        for p in pending:
+            p.cancel()
+
+        v_ids = []
+        c_ids = []
+        vid_data = []
+        channel_data = []
+        comment_data = []
+        tickers = []
+
+        for d in done:
+            try:
+                vid_ids, channel_ids, ticker, vids, subs, comments = d.result()
+                v_ids.extend(vid_ids)
+                c_ids.extend(channel_ids) 
+                vid_data.extend(vids)
+                channel_data.extend(subs)
+                if include_comments:
+                    comment_data.extend(comments)
+                tickers.extend(ticker)
+            except Exception as e:
+                print(f"Error retrieving data for query {e}")
+
+        return v_ids, c_ids, vid_data, channel_data, comment_data, tickers
+
